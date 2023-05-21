a0e3fbdff1702f164b365a76f13ce5a1e9160281
Andres6936 <andres6936@gmail.com>
2023-05-21 09:59:40 -0500

    Moved Components

--- src/routes/App.tsx
+++ src/routes/App.tsx
@@ -1,7 +1,7 @@
 import WaterTank from "../components/WaterTank.tsx";
 import ControlTanks from "../components/ControlTanks.tsx";
-import Footer from "../components/Footer.tsx";
-import Navigator from "../components/Navigator.tsx";
+import Footer from "../layout/Footer.tsx";
+import Navigator from "../layout/Navigator.tsx";
 import MainContainer from "../layout/MainContainer.tsx";
 
 export default function App() {
--- src/routes/Options.tsx
+++ src/routes/Options.tsx
@@ -1,6 +1,6 @@
 import OptionsTanks from "../components/OptionsTanks.tsx";
-import Navigator from "../components/Navigator.tsx";
-import Footer from "../components/Footer.tsx";
+import Navigator from "../layout/Navigator.tsx";
+import Footer from "../layout/Footer.tsx";
 import MainContainer from "../layout/MainContainer.tsx";
 
 export default function Options() {
