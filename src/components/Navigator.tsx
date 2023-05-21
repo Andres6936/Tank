a8bc90a265f9258174f5bf5f26d0ca71563298fb
Andres6936 <andres6936@gmail.com>
2023-05-21 09:07:49 -0500

    Added Icon

--- src/components/Navigator.tsx
+++ src/components/Navigator.tsx
@@ -1,9 +1,10 @@
+import IconGearWide from "../icons/IconGearWide.tsx";
+
 export default function Navigator() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-white border-bottom">
-            <div className="container-fluid">
-                <span className="navbar-brand text-muted mb-0 h1">Tank Utilities</span>
-            </div>
+        <nav className="flex flex:row p:1em bb:1px|solid|#CCC bg:white justify-content:space-between align-items:center">
+            <h1 className="navbar-brand text-muted mb-0 h1">Tank Utilities</h1>
+            <IconGearWide size={22} className={"opacity:0.8"}/>
         </nav>
     )
 }
--- src/types/Icon.ts
+++ src/types/Icon.ts
@@ -1,3 +1,4 @@
 export type IconProps = {
-    readonly size?: number
+    readonly size?: number;
+    readonly className?: string;
 }
