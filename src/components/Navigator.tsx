52c993d194382d3a5e094c97f267f2f72d77bbec
Andres6936 <andres6936@gmail.com>
2023-05-21 09:38:08 -0500

    Added Route

--- src/components/Navigator.tsx
+++ src/components/Navigator.tsx
@@ -1,10 +1,13 @@
 import IconGearWide from "../icons/IconGearWide.tsx";
+import {useNavigate} from "react-router-dom";
 
 export default function Navigator() {
+    const navigator = useNavigate();
+
     return (
         <nav className="flex flex:row p:1em bb:1px|solid|#CCC bg:white justify-content:space-between align-items:center">
-            <h1 className="navbar-brand text-muted mb-0 h1">Tank Utilities</h1>
-            <IconGearWide size={22} className={"opacity:0.8"}/>
+            <h1 onClick={() => navigator("/")} className="navbar-brand text-muted mb-0 h1">Tank Utilities</h1>
+            <IconGearWide onClick={() => navigator("/options")} size={22} className={"opacity:0.8"}/>
         </nav>
     )
 }
--- src/types/Icon.ts
+++ src/types/Icon.ts
@@ -1,4 +1,7 @@
+import type {MouseEventHandler} from "react";
+
 export type IconProps = {
     readonly size?: number;
     readonly className?: string;
+    readonly onClick?: MouseEventHandler<HTMLDivElement>;
 }
