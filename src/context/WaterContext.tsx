3e9a44b42d6a44cbe7c37e1fbc04f1cf8c44afec
Andres6936 <andres6936@gmail.com>
2023-05-21 19:42:04 -0500

    Added More Variables to Context

--- src/context/WaterContext.tsx
+++ src/context/WaterContext.tsx
@@ -1,13 +1,21 @@
 import {createContext, ReactNode, useState} from "react";
 
 export interface IWaterContext {
-    waterLevel: number
+    waterLevel: number,
+    maximumLevel: number,
+    minimumLevel: number,
+    setMaximumLevel: (value: number) => void,
+    setMinimumLevel: (value: number) => void,
     incrementWaterLevel: () => void,
     decrementWaterLevel: () => void,
 }
 
 export const WaterContext = createContext<IWaterContext>({
     waterLevel: 0,
+    maximumLevel: 500,
+    minimumLevel: 50,
+    setMaximumLevel: () => console.log("Not Implemented"),
+    setMinimumLevel: () => console.log("Not Implemented"),
     incrementWaterLevel: () => console.log("Not Implemented"),
     decrementWaterLevel: () => console.log("Not Implemented"),
 })
@@ -15,6 +23,8 @@ export const WaterContext = createContext<IWaterContext>({
 
 export default function WaterContextProvider (props: {children: ReactNode}) {
     const [waterLevel, setWaterLevel] = useState<number>(50)
+    const [maximumLevel, setMaximumLevel] = useState<number>(500);
+    const [minimumLevel, setMinimumLevel] = useState<number>(50);
     
     const incrementWaterLevel = () => setWaterLevel(waterLevel + 10);
     
@@ -23,6 +33,10 @@ export default function WaterContextProvider (props: {children: ReactNode}) {
     return (
         <WaterContext.Provider value={{
             waterLevel,
+            maximumLevel,
+            minimumLevel,
+            setMaximumLevel,
+            setMinimumLevel,
             incrementWaterLevel,
             decrementWaterLevel,
         }}>
