d70dc3a84bb8f691c85cc380bdde279c2dbf2642
Andres6936 <andres6936@gmail.com>
2023-05-23 08:59:32 -0500

    Added New Icons

---
+++ src/icons/IconThermometer.tsx
@@ -0,0 +1,14 @@
+import {IconProps} from "../types/Icon.ts";
+
+export default function IconThermometer(props: IconProps) {
+    return (
+        <div onClick={props.onClick} className={props.className ?? ''}>
+            <svg xmlns="http://www.w3.org/2000/svg" width={props.size ?? 16} height={props.size ?? 16}
+                 fill="currentColor" viewBox="0 0 16 16">
+                <path d="M8 14a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3z"/>
+                <path
+                    d="M8 0a2.5 2.5 0 0 0-2.5 2.5v7.55a3.5 3.5 0 1 0 5 0V2.5A2.5 2.5 0 0 0 8 0zM6.5 2.5a1.5 1.5 0 1 1 3 0v7.987l.167.15a2.5 2.5 0 1 1-3.333 0l.166-.15V2.5z"/>
+            </svg>
+        </div>
+    )
+}
