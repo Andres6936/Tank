2db668a276bc14264e58d599c5bacf1c2b5d1beb
Andres6936 <andres6936@gmail.com>
2023-05-23 09:15:05 -0500

    Added New Icon

--- src/components/WaterTank.tsx
+++ src/components/WaterTank.tsx
@@ -1,9 +1,11 @@
-import IconDroplet from "../icons/IconDroplet.tsx";
 import {useContext, useMemo} from "react";
 import {IWaterContext, WaterContext} from "../context/WaterContext.tsx";
 import {WaterLevel} from "../types/WaterLevel.ts";
-import IconGearWide from "../icons/IconGearWide.tsx";
 import {Thermostat} from "react-thermostat";
+import IconThermometer from "../icons/IconThermometer.tsx";
+import IconThermometerHalf from "../icons/IconThermometerHalf.tsx";
+import IconThermometerHigh from "../icons/IconThermometerHigh.tsx";
+import IconSpeedometer2 from "../icons/IconSpeedometer2.tsx";
 
 export default function WaterTank() {
     const waterContext = useContext<IWaterContext>(WaterContext);
@@ -35,20 +37,36 @@ export default function WaterTank() {
     const [leftGradiantFirstWave, rightGradiantFirstWave] = useMemo(() => {
         switch (getCurrentWaterLevel()) {
             case "high":
-                return ["#9d0208", "#d0000099"]
+                return ["#e40b0b", "#fae0e4"]
             case "medium":
-                return ["#0a5a8b", "#1582e999"]
+                return ["#023e8a", "#ade8f4"]
             case "low":
-                return ["#ffaa00", "#ff950099"];
+                return ["#ffd400", "#ffe97f"];
         }
     }, [waterContext.waterLevel])
 
     return (
         <section
             className={"flex flex:col flex-shrink:0 flex-basis:99.3% flex-basis:49.3%@sm flex-basis:33%@md p:0.5em py:2em"}>
-            <div className={"flex justify-content:center"}>
+            <div className={"flex position:relative justify-content:center"}>
+
+                <div className={"flex flex:col position:absolute top:5 right:15 gap:0.6em"}>
+                    <div className={"flex justify-content:center align-items:center b:2px|solid|gray w:3.2em h:3.2em r:50%"}>
+                        <IconThermometer className={"opacity:0.6"} size={40}/>
+                    </div>
+
+                    <div className={"flex justify-content:center align-items:center b:2px|solid|gray w:3.2em h:3.2em r:50%"}>
+                        <IconThermometerHalf className={"opacity:0.6"} size={40}/>
+                    </div>
+
+                    <div className={"flex justify-content:center align-items:center b:2px|solid|gray w:3.2em h:3.2em r:50%"}>
+                        <IconThermometerHigh className={"opacity:0.6"} size={40}/>
+                    </div>
+                </div>
+
                 <Thermostat
                     size={300}
+                    valueSuffix={"%"}
                     value={waterContext.waterLevel}
                     min={waterContext.minimumLevel}
                     max={waterContext.maximumLevel}
@@ -57,6 +75,12 @@ export default function WaterTank() {
                     colors: [rightGradiantFirstWave, leftGradiantFirstWave]
                     }}
                 />
+            </div>
+
+            <div className={"flex justify-content:center align-items:center mt:3em"}>
+                <div className={"flex justify-content:center align-items:center b:2px|solid|gray w:4em h:4em r:50%"}>
+                    <IconSpeedometer2 className={"opacity:0.6"} size={40}/>
+                </div>
             </div>
         </section>
     )
