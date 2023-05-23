6a9786b406efe720e3b65c7c9e568061c24b40e9
Andres6936 <andres6936@gmail.com>
2023-05-22 21:38:03 -0500

    Justify Content

--- src/layout/MainContainer.tsx
+++ src/layout/MainContainer.tsx
@@ -2,7 +2,7 @@ import {ReactNode} from "react";
 
 export default function MainContainer(props: {children: ReactNode}){
     return (
-        <main className="flex flex:wrap@sm min-h:85vh flex:col flex:row@sm gap:0.5em gap:0.4em@sm my:0.5em px:0.5em">
+        <main className="flex flex:wrap@sm justify-content:center min-h:85vh flex:col flex:row@sm gap:0.5em gap:0.4em@sm my:0.5em px:0.5em">
             {props.children}
         </main>
     )
