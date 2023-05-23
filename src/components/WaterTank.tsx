af8adbaf02064ead9a9960152f099a918a2747b5
Andres6936 <andres6936@gmail.com>
2023-05-23 06:58:48 -0500

    Moved Component

--- src/components/WaterTank.tsx
+++ src/components/WaterTank.tsx
@@ -36,19 +36,6 @@ export default function WaterTank() {
     return (
         <section
             className={"flex flex:col flex-shrink:0 flex-basis:99.3% flex-basis:49.3%@sm flex-basis:33%@md position:relative border p:0.5em bg-white shadow rounded"}>
-            <div className="flex flex:col gap:0.5em position:absolute top:15 left:15">
-                <CircularButton onClick={() => waterContext.incrementWaterLevel()}>
-                    <span className={"font:bold font-size:1.5em"}>+</span>
-                </CircularButton>
-                <CircularButton onClick={() => waterContext.decrementWaterLevel()}>
-                    <span className={"font:bold font-size:1.5em"}>-</span>
-                </CircularButton>
-            </div>
-
-            <button
-                className={"position:absolute text-align:center top:15 right:15 bg:#d00000 fg:white font:bold b:none r:50% w:3.5em h:3.5em"}>Drain
-            </button>
-
             <WaterAnimation className={"mt:1em"} backgroundColor={"white"} waterLevel={getCurrentWaterLevel()}/>
 
             <div
@@ -59,7 +46,20 @@ export default function WaterTank() {
                 <p className={"p:0 m:0 font-size:1.2em font:bold fg:#0096c7"}>{waterContext.waterLevel} Water Drops</p>
             </div>
 
-            <div className={"flex justify-content:center align-items:center my:1em"}>
+            <div className={"flex position:relative justify-content:center align-items:center my:1em"}>
+                <div className="flex flex:col gap:0.5em position:absolute bottom:0 right:7">
+                    <CircularButton onClick={() => waterContext.incrementWaterLevel()}>
+                        <span className={"font:bold font-size:1.5em"}>+</span>
+                    </CircularButton>
+                    <CircularButton onClick={() => waterContext.decrementWaterLevel()}>
+                        <span className={"font:bold font-size:1.5em"}>-</span>
+                    </CircularButton>
+                </div>
+
+                <button
+                    className={"position:absolute text-align:center top:0 right:5 bg:#d00000 fg:white font:bold b:none r:50% w:3.5em h:3.5em"}>Drain
+                </button>
+
                 <CircularSlider
                     min={waterContext.minimumLevel}
                     max={waterContext.maximumLevel}
