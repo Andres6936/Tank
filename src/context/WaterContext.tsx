d21a70c031d109dbfc8471a7d1767fb95cc5841b
Andres6936 <andres6936@gmail.com>
2023-05-23 08:47:51 -0500

    Change Default Values

--- src/context/WaterContext.tsx
+++ src/context/WaterContext.tsx
@@ -14,11 +14,11 @@ export interface IWaterContext {
 }
 
 export const WaterContext = createContext<IWaterContext>({
-    waterLevel: 350,
-    maximumLevel: 500,
-    minimumLevel: 50,
-    lowLevelAlarm: 200,
-    highLevelAlarm: 400,
+    waterLevel: 50,
+    maximumLevel: 100,
+    minimumLevel: 0,
+    lowLevelAlarm: 25,
+    highLevelAlarm: 80,
     setWaterLevel: () => console.log("Not Implemented"),
     setMaximumLevel: () => console.log("Not Implemented"),
     setMinimumLevel: () => console.log("Not Implemented"),
@@ -28,15 +28,15 @@ export const WaterContext = createContext<IWaterContext>({
 
 
 export default function WaterContextProvider (props: {children: ReactNode}) {
-    const [waterLevel, setWaterLevel] = useState<number>(350)
-    const [maximumLevel, setMaximumLevel] = useState<number>(500);
-    const [minimumLevel, setMinimumLevel] = useState<number>(50);
-    const [lowLevelAlarm, setLowLevelAlarm] = useState<number>(200);
-    const [highLevelAlarm, setHighLevelAlarm] = useState<number>(400);
+    const [waterLevel, setWaterLevel] = useState<number>(50)
+    const [maximumLevel, setMaximumLevel] = useState<number>(100);
+    const [minimumLevel, setMinimumLevel] = useState<number>(0);
+    const [lowLevelAlarm, setLowLevelAlarm] = useState<number>(25);
+    const [highLevelAlarm, setHighLevelAlarm] = useState<number>(80);
     
-    const incrementWaterLevel = () => setWaterLevel(waterLevel + 10);
+    const incrementWaterLevel = () => setWaterLevel(waterLevel + 5);
     
-    const decrementWaterLevel = () => setWaterLevel(waterLevel - 10);
+    const decrementWaterLevel = () => setWaterLevel(waterLevel - 5);
     
     return (
         <WaterContext.Provider value={{
