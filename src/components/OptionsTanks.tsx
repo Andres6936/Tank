ffe9e3c1683618940be93ee2637196aeb74beef6
Andres6936 <andres6936@gmail.com>
2023-05-24 07:27:28 -0500

    Change of Design

--- src/components/OptionsTanks.tsx
+++ src/components/OptionsTanks.tsx
@@ -6,11 +6,11 @@ export default function OptionsTanks() {
     const waterContext = useContext<IWaterContext>(WaterContext);
 
     return (
-        <div className="flex flex:col flex-shrink:0  flex-basis:99.3%@sm flex-basis:32.5%@md border bg-white shadow rounded">
-            <div className="px-4 py-2  mb-4 h-100">
+        <div className="flex flex:col flex-shrink:0  flex-basis:99.3%@xs flex-basis:50%@sm flex-basis:32.5%@md ">
+            <div className=" r:1em b:1px|solid|#CCC px-4 py-2  mb-4 h-100">
                 <div className="flex flex:col">
                     <form className="flex flex:col gap:0.7em pb:1em">
-                        <h4 className="col-12 small mb-3 pt-3 border-top">
+                        <h4 className="col-12 small mb-3 pt-3">
                             Capacity
                         </h4>
                         <InputLabel value={waterContext.maximumLevel} label={"Maximum Liters"}/>
@@ -26,36 +26,9 @@ export default function OptionsTanks() {
                         <InputLabel value={waterContext.lowLevelAlarm} label={"Low Level Liters"}/>
                     </form>
                 </div>
-
-                <div className="col-12 btn-group" role="group">
-                    <button
-                        className="btn btn-primary btn-sm"
-                        id="button-edit"
-                        type="button"
-                        onclick="switchEnableDisableInput()"
-                        >
-                        Edit
-                    </button>
-                    <button
-                        className="btn btn-primary btn-sm"
-                        id="button-cancel"
-                        type="button"
-                        onclick="cancelChangesInCapacityAndAlarm()"
-                        disabled=""
-                        >
-                        Cancel
-                    </button>
-                    <button
-                        className="btn btn-primary btn-sm"
-                        id="button-confirm"
-                        type="button"
-                        onclick="confirmChangeInCapacityAndAlarm()"
-                        disabled=""
-                        >
-                        Confirm
-                    </button>
-                </div>
             </div>
+
+            <button className="bg:blue-50 fg:white font:bold py:0.7em r:1.2em mx:1em mt:1em b:none">Apply Changes</button>
         </div>
     )
 }
