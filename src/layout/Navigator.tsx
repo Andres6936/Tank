d0cb825ebf1c099e59af3109dd5dcaa7e0946ad4
Andres6936 <andres6936@gmail.com>
2023-05-25 17:39:49 -0500

    Added Styles

--- src/layout/Navigator.tsx
+++ src/layout/Navigator.tsx
@@ -9,14 +9,15 @@ export default function Navigator() {
     const renderIconBack = () => {
         if (location.pathname === "/options") {
             return (
-                <div onClick={() => navigator("/")}
-                     className={"flex bg:white justify-content:center align-items:center  b:1px|solid|gray w:2.5em h:2.5em r:50%"}>
-                    <IconChevronLeft size={22} className="opacity:0.6"/>
+                <div className={"flex flex:1 justify-content:center"}>
+                    <IconChevronLeft
+                        onClick={() => navigator("/")} size={22}
+                        className="flex bg:white justify-content:center align-items:center  b:1px|solid|gray w:2.5em h:2.5em r:50% opacity:0.6"/>
                 </div>
             )
         } else {
             return (
-                <div className={"flex"}/>
+                <div className={"flex flex:1"}/>
             )
         }
     }
@@ -24,14 +25,16 @@ export default function Navigator() {
     const renderIconOptions = () => {
         if (location.pathname === "/") {
             return (
-                <div onClick={() => navigator("/options")}
-                     className={"flex bg:white justify-content:center align-items:center  b:1px|solid|gray w:2.5em h:2.5em r:50%"}>
-                    <IconGearWide size={22} className={"opacity:0.6"}/>
+                <div className={"flex flex:1 justify-content:center"}>
+                    <IconGearWide
+                        onClick={() => navigator("/options")} size={22}
+                        className={"flex bg:white justify-content:center align-items:center  b:1px|solid|gray w:2.5em h:2.5em r:50% opacity:0.6"}/>
+
                 </div>
             )
         } else {
             return (
-                <div className={"flex"}/>
+                <div className={"flex flex:1"}/>
             )
         }
     }
@@ -40,7 +43,7 @@ export default function Navigator() {
         <nav className="flex flex:row p:1em justify-content:space-between align-items:center">
             {renderIconBack()}
 
-            <div className={"flex flex:1 flex:col justify-content:center"}>
+            <div className={"flex flex:6 flex:col justify-content:center"}>
                 <h5 className={"text-align:center p:0 m:0 opacity:0.8"}>Water Tank</h5>
                 <p className={"text-align:center p:0 m:0 opacity:0.8 lh:0.6em"}>Monitory</p>
             </div>
