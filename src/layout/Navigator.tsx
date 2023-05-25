66fb98abce03e2954c11e9fa712bfefed433f9f2
Andres6936 <andres6936@gmail.com>
2023-05-25 11:33:00 -0500

    Moved OnClick

--- src/layout/Navigator.tsx
+++ src/layout/Navigator.tsx
@@ -7,8 +7,9 @@ export default function Navigator() {
 
     return (
         <nav className="flex flex:row p:1em justify-content:space-between align-items:center">
-            <div className={"flex bg:white justify-content:center align-items:center  b:1px|solid|gray w:2.5em h:2.5em r:50%"}>
-                <IconChevronLeft onClick={() => navigator("/")} size={22} className="opacity:0.6"/>
+            <div  onClick={() => navigator("/")}
+                className={"flex bg:white justify-content:center align-items:center  b:1px|solid|gray w:2.5em h:2.5em r:50%"}>
+                <IconChevronLeft size={22} className="opacity:0.6"/>
             </div>
 
             <div className={"flex flex:col justify-content:center"}>
@@ -16,8 +17,9 @@ export default function Navigator() {
                 <p className={"text-align:center p:0 m:0 opacity:0.8 lh:0.6em"}>Monitory</p>
             </div>
 
-            <div className={"flex bg:white justify-content:center align-items:center  b:1px|solid|gray w:2.5em h:2.5em r:50%"}>
-                <IconGearWide onClick={() => navigator("/options")} size={22} className={"opacity:0.6"}/>
+            <div onClick={() => navigator("/options")}
+                className={"flex bg:white justify-content:center align-items:center  b:1px|solid|gray w:2.5em h:2.5em r:50%"}>
+                <IconGearWide size={22} className={"opacity:0.6"}/>
             </div>
         </nav>
     )
