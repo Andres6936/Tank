5e9f61bb0a6d79056767b9cb697fc0e02013fb97
Andres6936 <andres6936@gmail.com>
2023-05-25 16:37:49 -0500

    Added New Icon

--- src/components/WaterTank.tsx
+++ src/components/WaterTank.tsx
@@ -6,6 +6,7 @@ import IconThermometer from "../icons/IconThermometer.tsx";
 import IconThermometerHalf from "../icons/IconThermometerHalf.tsx";
 import IconThermometerHigh from "../icons/IconThermometerHigh.tsx";
 import IconSpeedometer2 from "../icons/IconSpeedometer2.tsx";
+import IconFlower1 from "../icons/IconFlower1.tsx";
 
 export default function WaterTank() {
     const waterContext = useContext<IWaterContext>(WaterContext);
@@ -49,6 +50,14 @@ export default function WaterTank() {
         <section
             className={"flex flex:col flex-shrink:0 flex-basis:99.3% flex-basis:49.3%@sm flex-basis:33%@md p:0.5em py:2em"}>
             <div className={"flex position:relative justify-content:center"}>
+
+                <div className={"flex flex:col position:absolute top:5 left:15 gap:0.6em"}>
+                    <p className={"m:0 p:0 font-size:4em font:sans font:thin line-height:1.5em"}>15</p>
+
+                    <div className={"flex justify-content:center align-items:center b:2px|solid|gray w:3.2em h:3.2em r:50%"}>
+                        <IconFlower1 className={"opacity:0.6"} size={28}/>
+                    </div>
+                </div>
 
                 <div className={"flex flex:col position:absolute top:5 right:15 gap:0.6em"}>
                     <div className={"flex justify-content:center align-items:center b:2px|solid|gray w:3.2em h:3.2em r:50%"}>
