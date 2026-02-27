09719e82fae26aee0789a065b82b0910ef3022ed
Andres6936 <andres6936@gmail.com>
2026-02-26 20:52:46 -0500

    Increase max width of WaterTank and fix MainContainer flex
    - WaterTank max width increased from 35rem to 45rem
    - MainContainer now uses flex:1 only on small screens and above

--- src/layout/MainContainer.tsx
+++ src/layout/MainContainer.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 
 export default function MainContainer(props: { children: ReactNode }) {
   return (
-    <main className="flex flex:1 flex:wrap@sm justify-content:center@sm align-items:center flex-col flex:row@sm gap:0.5em gap:0.4em@sm">
+    <main className="flex flex:1@sm flex:wrap@sm justify-content:center@sm align-items:center flex-col flex:row@sm gap:0.5em gap:0.4em@sm">
       {props.children}
     </main>
   );
