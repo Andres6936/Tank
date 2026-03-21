0b1eaf2428849669d0c288946360199b24c01b81
Andres6936 <andres6936@gmail.com>
2026-03-21 11:01:53 -0500

    Rename proposal component to CLI and remove unused index file

--- proposals/index.ts
+++
@@ -1,8 +0,0 @@
-import "konva/canvas-backend";
-import { getBufferSeals } from "./pdf/utility/buffer-seals";
-
-const buffers = await getBufferSeals({
-  seal: "blue",
-});
-
-await Bun.write("out.png", buffers.seal);
