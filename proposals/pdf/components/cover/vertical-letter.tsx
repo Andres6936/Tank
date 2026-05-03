09aa352e6f9241429f9ca247d1f3706ac89e91f9
Andres6936 <andres6936@gmail.com>
2026-05-03 08:00:37 -0500

    Format vertical-letter components

--- proposals/pdf/components/cover/vertical-letter.tsx
+++ proposals/pdf/components/cover/vertical-letter.tsx
@@ -1,54 +1,50 @@
 import React from "react";
-import { Text, View } from "@react-pdf/renderer"
-import { mergeStyles } from "~/pdf/utility/merge-props"
+import { Text, View } from "@react-pdf/renderer";
+import { mergeStyles } from "~/pdf/utility/merge-props";
 
 function Root(props: React.ComponentPropsWithRef<typeof View>) {
-    return (
-        <View
-            {...props}
-            style={mergeStyles(
-                {
-                    position: "absolute",
-                    bottom: "50%",
-                    right: "-3cm",
-                    flexDirection: "column",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    transform: "rotate(270deg)",
-                },
-                props.style
-            )}
-        />
-    )
+  return (
+    <View
+      {...props}
+      style={mergeStyles(
+        {
+          position: "absolute",
+          bottom: "50%",
+          right: "-3cm",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          transform: "rotate(270deg)",
+        },
+        props.style,
+      )}
+    />
+  );
 }
 
 function Line(props: React.ComponentPropsWithRef<typeof Text>) {
-    return (
-        <Text
-            {...props}
-            style={mergeStyles(
-                {
-                    textTransform: "uppercase",
-                    fontSize: "7pt",
-                    opacity: "0.7",
-                    letterSpacing: "2pt",
-                },
-                props.style
-            )}
-        />
-    )
+  return (
+    <Text
+      {...props}
+      style={mergeStyles(
+        {
+          textTransform: "uppercase",
+          fontSize: "7pt",
+          opacity: "0.7",
+          letterSpacing: "2pt",
+        },
+        props.style,
+      )}
+    />
+  );
 }
 
 function VerticalLetter(props: React.PropsWithChildren<{}>) {
-    return (
-        <Root>
-            <Line>{props.children}</Line>
-        </Root>
-    )
+  return (
+    <Root>
+      <Line>{props.children}</Line>
+    </Root>
+  );
 }
 
-export {
-    Root,
-    Line,
-    VerticalLetter,
-}
+export { Root, Line, VerticalLetter };
