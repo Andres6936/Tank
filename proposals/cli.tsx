0d8a6ea5698e2da19fdacdfb240195714808e9d5
Andres6936 <andres6936@gmail.com>
2026-05-03 07:48:24 -0500

    Reorder imports in proposals/cli.tsx

--- proposals/cli.tsx
+++ proposals/cli.tsx
@@ -2,11 +2,12 @@ import ReactPDF from "@react-pdf/renderer";
 
 // Argument Parser
 import { optional, withDefault, map } from "@optique/core/modifiers";
+import { string, choice } from "@optique/core/valueparser";
+import { message } from "@optique/core/message";
 import { object } from "@optique/core/constructs";
 import { option } from "@optique/core/primitives";
-import { string, choice } from "@optique/core/valueparser";
+import { run, print } from "@optique/run";
 import { path } from "@optique/run/valueparser";
-import { run } from "@optique/run";
 
 // Utility Seals Buffers
 import { getBufferSeals } from "~/pdf/utility/buffer-seals";
@@ -41,4 +42,5 @@ const config = run(parser, {
   });
   const document = await proposal({ file, buffers });
   ReactPDF.render(document, output);
+  print(message`PDF generated successfully to ${output}`);
 })();
