0ab58ba59d6860fad2ac52086994d14be701054c
Andres6936 <andres6936@gmail.com>
2026-05-03 13:19:03 -0500

    Refactor fromSvg to delegate conversion to svgToSvg

--- proposals/pdf/svg/index.tsx
+++ proposals/pdf/svg/index.tsx
@@ -1,33 +1,11 @@
 import React from "react";
-import { Svg, type SVGProps } from "@react-pdf/renderer";
-import { getAttributes, getChildren } from "./from-svg";
+import { svgToSvg } from "./from-svg";
 
-/**
- * svg to Svg
- * @param svg SVG
- * @returns ReactPDF Svg
- */
-const fromSvg = (svg: SVGElement): React.JSX.Element => {
+const fromSvg = (svg: Element): React.JSX.Element => {
   if (!svg || svg.tagName !== "svg")
     throw new Error("Your element is not a svg element");
 
-  // Props
-  const props: SVGProps = {};
-  getAttributes(
-    svg,
-    [
-      { key: "width", type: "number" },
-      { key: "height", type: "number" },
-      { key: "viewBox", type: "string" },
-      { key: "preserveAspectRatio", type: "number" },
-    ],
-    props,
-  );
-
-  /**
-   * Render
-   */
-  return <Svg {...props}>{getChildren(svg)}</Svg>;
+  return svgToSvg(svg);
 };
 
 export { fromSvg };
