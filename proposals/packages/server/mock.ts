456d1b5f0546e2cbe601483aed0d2b3efc7cef09
Andres6936 <andres6936@gmail.com>
2026-05-06 20:09:17 -0500

    Update mock API call to use DELETE and new document ID

--- proposals/packages/server/mock.ts
+++ proposals/packages/server/mock.ts
@@ -17,10 +17,47 @@
 //   console.error(error);
 // }
 
+// const result = await fetch(
+//   "http://localhost:3000/api/documents/private/019dffe7-0b3d-7c94-995d-963eb7a64795",
+//   {
+//     method: "GET",
+//   },
+// );
+
+// if (result.ok) {
+//   const data = await result.json();
+//   console.log(data);
+// } else {
+//   const error = await result.json();
+//   console.error(error);
+// }
+
+// const file = Bun.file("README.md");
+
+// const formData = new FormData();
+// formData.append("Path", "/README.md");
+// formData.append("Blob", file);
+
+// const result = await fetch(
+//   "http://localhost:3000/api/documents/private/019dffe7-0b3d-7c94-995d-963eb7a64795",
+//   {
+//     method: "PUT",
+//     body: formData,
+//   },
+// );
+
+// if (result.ok) {
+//   const data = await result.json();
+//   console.log(data);
+// } else {
+//   const error = await result.json();
+//   console.error(error);
+// }
+
 const result = await fetch(
-  "http://localhost:3000/api/documents/private/019dff54-4d91-70af-b858-eb71b45341a8",
+  "http://localhost:3000/api/documents/private/019dffe7-0b3d-7c94-995d-963eb7a64795",
   {
-    method: "GET",
+    method: "DELETE",
   },
 );
 
