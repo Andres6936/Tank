36b252f68bdc981c08fa0dc2d3f62c075181f95e
Andres6936 <andres6936@gmail.com>
2026-05-07 20:41:55 -0500

    Add explicit column names to default schemas

--- apps/server/src/db/default.ts
+++ apps/server/src/db/default.ts
@@ -1,11 +1,11 @@
 import { sql } from "drizzle-orm";
 import { text } from "drizzle-orm/sqlite-core";
 
-const defaultId = text()
+const defaultId = text("Id")
   .primaryKey()
   .default(sql`(uuid7())`);
 
-const defaultISODate = text()
+const defaultISODate = text("CreatedAt")
   .notNull()
   .default(sql`(time_fmt_iso(time_now()))`);
 
