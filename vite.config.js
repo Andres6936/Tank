f01e03412809ac06ed5e99bb99c18c481eb8d985
Andres6936 <andres6936@gmail.com>
2026-08-17 20:33:16 -0500

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import masterCSS from "@master/css.vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react(), masterCSS()],
-});
