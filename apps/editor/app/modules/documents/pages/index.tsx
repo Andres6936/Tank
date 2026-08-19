ae1035b9dd927548a01a272483244849d44685e6
Andres6936 <andres6936@gmail.com>
2026-08-19 13:30:13 -0500

    Add document cards to documents page

--- apps/editor/app/modules/documents/pages/index.tsx
+++ apps/editor/app/modules/documents/pages/index.tsx
@@ -1,3 +1,18 @@
+const Document = () => {
+  return (
+    <div>
+      <div className="w-[10rem] aspect-[16/23] rounded border bg-zinc-200"></div>
+      <p className="text-xs/4 text-balance text-center mt-2 px-2">
+        Asistente Conversacional y Operativo (IA)
+      </p>
+    </div>
+  );
+};
+
 export default function Page() {
-  return <p>Hi</p>;
+  return (
+    <div className="grid grid-cols-[repeat(auto-fill,160px)] gap-4 place-content-start">
+      <Document /> <Document /> <Document />
+    </div>
+  );
 }
