b2d64856adce2707bf30e54ccd3fa2ff8dc92e94
Andres6936 <andres6936@gmail.com>
2026-08-19 14:42:47 -0500

    Remove editor background colors and fix iframe layout

--- apps/editor/app/modules/documents/pages/view.tsx
+++ apps/editor/app/modules/documents/pages/view.tsx
@@ -3,16 +3,18 @@ import { XmlEditor } from "~/components/Editor";
 export default function View() {
   return (
     <div className="flex flex-1 flex-row gap-2">
-      <div className="relative flex flex-1 bg-yellow-100">
+      <div className="relative flex flex-1">
         <div className="absolute inset-0 flex-1">
           <XmlEditor />
         </div>
       </div>
-      <div className="flex flex-1 bg-sky-100">
-        <iframe
-          src="https://www.rd.usda.gov/sites/default/files/pdf-sample_0.pdf"
-          className="w-full h-full border border-zinc-200 hover:border-zinc-400 focus:border-zinc-400"
-        ></iframe>
+      <div className="relative flex flex-1">
+        <div className="absolute inset-0 flex-1 border border-zinc-300">
+          <iframe
+            src="https://www.rd.usda.gov/sites/default/files/pdf-sample_0.pdf"
+            className="w-full h-full"
+          />
+        </div>
       </div>
     </div>
   );
