e6f0e074e6cfb60ef52017d78d3cc10a07ce6598
Andres6936 <andres6936@gmail.com>
2026-08-19 00:05:12 -0500

    Add server dev scripts and document router

--- apps/server/src/server/router.ts
+++ apps/server/src/server/router.ts
@@ -2,12 +2,14 @@ import { asPayload } from "~/utility/response";
 import { publicProcedure, router } from "./trpc";
 
 import files from "~/files/router";
+import documents from "~/documents/router";
 
 export const app = router({
   status: publicProcedure.query(async () => {
     return asPayload(200, { message: "OK" });
   }),
   files,
+  documents,
 });
 
 export type AppRouter = typeof app;
