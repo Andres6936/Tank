de86320dab9fdb38df8e88c67794ab74ea0c5a3a
Andres6936 <andres6936@gmail.com>
2026-08-24 10:09:39 -0500

    Add Documents and TypeStateDocument tables

---
+++ apps/server/drizzle/0001_flashy_omega_red.sql
@@ -0,0 +1,18 @@
+CREATE TABLE `Documents` (
+	`Id` text PRIMARY KEY DEFAULT (uuid7()) NOT NULL,
+	`Title` text NOT NULL,
+	`Subject` text NOT NULL,
+	`Content` text NOT NULL,
+	`TypeState` text NOT NULL,
+	`FileId` text,
+	`Metadata` text DEFAULT '{}' NOT NULL,
+	`CreatedAt` text DEFAULT (time_fmt_iso(time_now())) NOT NULL,
+	FOREIGN KEY (`TypeState`) REFERENCES `TypeStateDocument`(`Type`) ON UPDATE no action ON DELETE no action,
+	FOREIGN KEY (`FileId`) REFERENCES `Files`(`Id`) ON UPDATE no action ON DELETE cascade
+);
+--> statement-breakpoint
+CREATE INDEX `Document_FileId` ON `Documents` (`FileId`);--> statement-breakpoint
+CREATE TABLE `TypeStateDocument` (
+	`Type` text PRIMARY KEY NOT NULL,
+	`Metadata` text DEFAULT '{}' NOT NULL
+);
