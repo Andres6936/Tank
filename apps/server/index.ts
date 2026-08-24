0bfaa38a32d89a3f1efc622544c8e6c3b4ce600e
Andres6936 <andres6936@gmail.com>
2026-08-24 18:39:41 -0500

    Set trpc CORS headers on response directly

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -8,15 +8,14 @@ const server = Bun.serve({
     "/api/auth/*": (r) => auth.handler(r),
     "/trpc/*": async (request) => {
       const response = await handler(request);
-      const overwrite = new Response(response.body, response);
       const cors = {
         "Access-Control-Allow-Origin": request.headers.get("Origin") || "*",
         "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
       };
       for (const [key, value] of Object.entries(cors)) {
-        overwrite.headers.set(key, value);
+        response.headers.set(key, value);
       }
-      return overwrite;
+      return response;
     },
   },
 });
