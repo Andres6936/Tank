d2f1920df23331b619b476aaf7e5b06a08d5abb8
Andres6936 <andres6936@gmail.com>
2026-08-25 10:48:22 -0500

    Add "See more" button to document list

--- apps/editor/app/modules/documents/components/infinity-list.tsx
+++ apps/editor/app/modules/documents/components/infinity-list.tsx
@@ -1,7 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
+import { Plus } from "lucide-react";
 
 import { useTRPC } from "~/utils/trpc";
 import { Document } from "./document";
+import { Button } from "~/components/ui/button";
 
 const InfinityList = () => {
   const trpc = useTRPC();
@@ -22,10 +24,17 @@ const InfinityList = () => {
   const items = query.data.body;
 
   return (
-    <div className="grid grid-cols-[repeat(auto-fill,10rem)] gap-4 place-content-start">
-      {items.map((item) => (
-        <Document key={item.Id} document={item} />
-      ))}
+    <div className="flex flex-col flex-1">
+      <div className="grid grid-cols-[repeat(auto-fill,10rem)] gap-4 place-content-start ">
+        {items.map((item) => (
+          <Document key={item.Id} document={item} />
+        ))}
+      </div>
+      <div className="mt-auto self-center">
+        <Button variant="outline">
+          <Plus size={18} strokeWidth={1.5} /> See more
+        </Button>
+      </div>
     </div>
   );
 };
