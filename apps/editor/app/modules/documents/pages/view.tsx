11369169dacbb130e0615a0f8262996b64739a4c
Andres6936 <andres6936@gmail.com>
2026-09-03 18:19:17 -0500

    Add toolbar buttons to document view

--- apps/editor/app/modules/documents/pages/view.tsx
+++ apps/editor/app/modules/documents/pages/view.tsx
@@ -1,5 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
+import {
+  Braces,
+  CloudCheck,
+  FileDown,
+  Settings,
+  Signature,
+  Stamp,
+} from "lucide-react";
+
 import { XmlEditor } from "~/components/Editor";
+import { Button } from "~/components/ui/button";
+import { ButtonGroup } from "~/components/ui/button-group";
 import { useTRPC } from "~/utils/trpc";
 import { Preview } from "../components/preview";
 
@@ -27,14 +38,45 @@ export default function View({ Id }: { Id: string }) {
 
   return (
     <div className="flex flex-1 flex-row gap-2">
-      <div className="relative flex flex-1">
-        <div className="absolute inset-0 flex-1">
-          <XmlEditor content={item.Content} />
+      <div className="flex flex-col flex-1 space-y-2">
+        <ButtonGroup>
+          <Button variant="outline" size="icon">
+            <Braces />
+          </Button>
+          <Button variant="outline" size="icon">
+            <Signature />
+          </Button>
+        </ButtonGroup>
+        <div className="relative flex flex-1">
+          <div className="absolute inset-0 flex-1">
+            <XmlEditor content={item.Content} />
+          </div>
         </div>
       </div>
-      <div className="relative flex flex-1">
-        <div className="absolute inset-0 flex-1 border border-zinc-300">
-          <Preview content={item.Content} />
+      <div className="flex flex-col flex-1 space-y-2">
+        <div className="flex justify-between">
+          <ButtonGroup>
+            <Button variant="outline" size="icon">
+              <CloudCheck />
+            </Button>
+            <Button variant="outline" size="icon">
+              <FileDown />
+            </Button>
+          </ButtonGroup>
+
+          <ButtonGroup>
+            <Button variant="outline" size="icon">
+              <Stamp />
+            </Button>
+            <Button variant="outline" size="icon">
+              <Settings />
+            </Button>
+          </ButtonGroup>
+        </div>
+        <div className="relative flex flex-1">
+          <div className="absolute inset-0 flex-1 border border-zinc-300">
+            <Preview content={item.Content} />
+          </div>
         </div>
       </div>
     </div>
