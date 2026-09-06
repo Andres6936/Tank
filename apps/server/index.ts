ef77badfa211665b7ac4aa0e807819b851761179
Andres6936 <andres6936@gmail.com>
2026-09-06 13:50:24 -0500

    Configure server port from environment variable

--- apps/server/index.ts
+++ apps/server/index.ts
@@ -2,7 +2,7 @@ import { auth } from "./src/lib/auth";
 import { handler } from "./src/server";
 
 const server = Bun.serve({
-  port: 3000,
+  port: process.env.SERVER_PORT,
   routes: {
     "/api/status": new Response("OK"),
     "/api/auth/*": (r) => auth.handler(r),
