40abb64bc872282926f1f095c4339c5e5c21a76f
Andres6936 <andres6936@gmail.com>
2026-09-14 11:19:15 -0500

    Use Bun global for S3 clients

--- apps/server/src/config/clients-vault.ts
+++ apps/server/src/config/clients-vault.ts
@@ -1,26 +1,24 @@
-import { S3Client } from "bun";
-
 const BucketsAvailable = {
   Private: "private",
   Ephemeral: "ephemeral",
 } as const;
 
 const getVaultsClients = () => {
-  const publicVault = new S3Client({
+  const publicVault = new Bun.S3Client({
     endpoint: process.env.R2_PUBLIC_ENDPOINT,
     accessKeyId: process.env.R2_PUBLIC_ACCESS_KEY_ID,
     secretAccessKey: process.env.R2_PUBLIC_ACCESS_SECRET_KEY,
     bucket: "public",
   });
 
-  const privateVault = new S3Client({
+  const privateVault = new Bun.S3Client({
     endpoint: "https://s3sea.andres6936.dev/",
     accessKeyId: process.env.SEAWEEDFS_ACCESS_KEY_ID,
     secretAccessKey: process.env.SEAWEEDFS_ACCESS_SECRET_KEY,
     bucket: "private",
   });
 
-  const ephemeralVault = new S3Client({
+  const ephemeralVault = new Bun.S3Client({
     endpoint: "https://s3sea.andres6936.dev/",
     accessKeyId: process.env.SEAWEEDFS_ACCESS_KEY_ID,
     secretAccessKey: process.env.SEAWEEDFS_ACCESS_SECRET_KEY,
